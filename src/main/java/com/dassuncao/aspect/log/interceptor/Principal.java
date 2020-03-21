94a28cb5d2c3cd83a1498b7f5d58e6999a1120cd
Danilo Assunção <dassuncao@uolinc.com>
2020-03-21 12:51:30 -0300

    Removed unusual classes

--- src/main/java/com/dassuncao/aspect/log/interceptor/Principal.java
+++
@@ -1,8 +0,0 @@
-package com.dassuncao.aspect.log.interceptor;
-
-public class Principal {
-
-    public static void main(String[] args) {
-        new MethodSample().method("DANILO", "ASSUNCAO");
-    }
-}
